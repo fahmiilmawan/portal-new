7ef57e39610e94e604f573c7cc41a78e9d61cbf3
fahmiilmawan <fahmikeilmawan22@gmail.com>
2021-08-12 22:35:52 +0700

    judul

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>SMK LPPM RI BANDUNG</title>
+  <title>SMK LPPM RI BONANG</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
